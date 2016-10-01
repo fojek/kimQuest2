05cfbab617734a3143515594f275b57a90d4d1f9
Fojek <marc.fortier.1@gmail.com>
2016-09-30 23:00:58 -0400

    Version compilable, moteur fonctionnel Ajout fichier excel pour générer automatiquement les tableaux

--- main.cpp
+++ main.cpp
@@ -20,35 +20,64 @@ typedef struct
 int main()
 {
     // Fonctions
-    int dessinerTableau(TABLEAU tableau); // Fonction pour dessiner un tableau
-    int chargerTableaux(TABLEAU * tableau);
+    int dessinerTableau(TABLEAU); // Fonction pour dessiner un tableau
+    int chargerTableaux(TABLEAU *);
     void effacerEcran();
+    int effectuerAction(TABLEAU *, int, int);
 
     // Constantes
     const COORD POS_ACTION = {10,25};
     const int TABLEAU_DEPART = 1;
 
     // Variables
-    TABLEAU tableauActuel;
+    int tableauActuel;
     int choix;
 
-    TABLEAU tableau[10];
+    TABLEAU tableau[100];
 
-    tableauActuel.idTableau = TABLEAU_DEPART;
+    tableauActuel = TABLEAU_DEPART;
 
     // 1. Charger les tableaux en mémoire
+    chargerTableaux(tableau);
+
     // 2. Algorithme du jeu :
     //  2.1 Afficher le tableau
-
-    effacerEcran();
-    chargerTableaux(tableau);
-    dessinerTableau(tableau[tableauActuel.idTableau]);
-
-    cin >> choix;
-    //  2.2 Demander l'action
-    //  2.3 Changer le tableau actif
+    do
+    {
+        dessinerTableau(tableau[tableauActuel]);
+        cin >> choix;
+        tableauActuel = effectuerAction(tableau, tableauActuel, choix);
+    } while (tableauActuel!=99);
 
     return 0;
+}
+
+int effectuerAction(TABLEAU * tabl, int tablActuel, int chx)
+{
+    // Cas spécial, pour un tableau sans choix
+    if(tabl[tablActuel].lienChoix[0])
+        return tabl[tablActuel].lienChoix[0];
+
+    // On vérifie si le choix existe au tabl actuel
+    // Si !=0, on assigne le nouveau tabl
+    switch (chx)
+    {
+    case 1:
+        if(tabl[tablActuel].lienChoix[1])
+            return tabl[tablActuel].lienChoix[1];
+    case 2:
+        if(tabl[tablActuel].lienChoix[2])
+            return tabl[tablActuel].lienChoix[2];
+    case 3:
+        if(tabl[tablActuel].lienChoix[3])
+            return tabl[tablActuel].lienChoix[3];
+    case 4:
+        if(tabl[tablActuel].lienChoix[4])
+            return tabl[tablActuel].lienChoix[4];
+    }
+
+    // Aucun choix valide, on recharge le même tableau
+    return tablActuel;
 }
 
 void effacerEcran() {
@@ -66,14 +95,17 @@ void effacerEcran() {
 int dessinerTableau(TABLEAU tableau)
 {
     // Fonctions
+    void effacerEcran();
 
     // Variables
     //short pos1=0, pos2=15;
     //COORD posTitre = {8, 0};
     CONSOLE_SCREEN_BUFFER_INFO s;
-    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 
     // Initialisation
+    effacerEcran();
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+
     GetConsoleScreenBufferInfo(console, &s);
 
     // Algorithme
@@ -116,51 +148,204 @@ int dessinerTableau(TABLEAU tableau)
 int chargerTableaux(TABLEAU * tableau)
 {
     int numT=0; // index pour les tableaux
-
     //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
     // Definition du tableau 1
+    numT++;
     tableau[numT].idTableau = numT; // numT = 0
-    tableau[numT].lienChoix[0] = 1;
+    tableau[numT].lienChoix[0] = 0;
     tableau[numT].lienChoix[1] = 2;
-    tableau[numT].lienChoix[2] = 3;
-    tableau[numT].lienChoix[3] = 4;
-    tableau[numT].lienChoix[4] = 5;
+    tableau[numT].lienChoix[2] = 99;
+    tableau[numT].lienChoix[3] = 0;
+    tableau[numT].lienChoix[4] = 0;
     tableau[numT].titre = "Prologue";
-    tableau[numT].texteParag[0] = "   Texte du chapitre. Cette partie doit \210tre bien format\202e et surtout";
-    tableau[numT].texteParag[1] = "ne pas contenir d'accents ou de caract\212res weird. Kim Pendragon se";
-    tableau[numT].texteParag[2] = "promenait dans la for\210t en fredonnant un air a la mode. Soudain, son";
-    tableau[numT].texteParag[3] = "attention fut retenue par un bruit \202trange provenant d'un buisson sur";
-    tableau[numT].texteParag[4] = "sa gauche.";
+    tableau[numT].texteParag[0] = "   Il était une fois, dans une contrée lointaine peuplée d'étranges";
+    tableau[numT].texteParag[1] = "créatures, une petit royaume juché au sommet d'une colline escarpée";
+    tableau[numT].texteParag[2] = "autant que fleurie, doté d'un nom que personne n'utilisait, lui pré-";
+    tableau[numT].texteParag[3] = "férant celui de Fleurimont. Sur ce cadre enchanteur se lève le ri-";
+    tableau[numT].texteParag[4] = "deau de l'histoire fantastique dont VOUS SEREZ L'HÉROINE ! ";
     tableau[numT].texteParag[5] = "";
-    tableau[numT].texteChoix[0] = "1. N'\202coutant que son courage, elle \202carte les branches ..";
-    tableau[numT].texteChoix[1] = "2. Consid\202rant la possible pr\202sence d'araign\202es, elle vaporise";
-    tableau[numT].texteChoix[2] = "   une quantit\202 d\202raisonable de Raid dans le buisson ..";
-    tableau[numT].texteChoix[3] = "3. Interdite, elle recule lentement sans regarder derri\212re elle ..";
+    tableau[numT].texteChoix[0] = "1. Youpie! Qu'est-ce qu'on attend ?";
+    tableau[numT].texteChoix[1] = "2. Ça fait peur ... Je crois que je vais me désister couardement.";
+    tableau[numT].texteChoix[2] = "";
+    tableau[numT].texteChoix[3] = "";
     tableau[numT].texteChoix[4] = "";
-    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-
 
     //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
     // Definition du tableau 2
     numT++;
     tableau[numT].idTableau = numT; // numT = 1
+    tableau[numT].lienChoix[0] = 0;
+    tableau[numT].lienChoix[1] = 3;
+    tableau[numT].lienChoix[2] = 0;
+    tableau[numT].lienChoix[3] = 0;
+    tableau[numT].lienChoix[4] = 0;
+    tableau[numT].titre = "An unexpected journey";
+    tableau[numT].texteParag[0] = "   Kim Pendragon se promenait dans la forêt en fredonnant un air à";
+    tableau[numT].texteParag[1] = "la mode. Elle était accompagné de ses pages: l'un, grand et mince, ";
+    tableau[numT].texteParag[2] = "se nommait Joseph le Brave; L'autre, petit et dodu, avait pour nom";
+    tableau[numT].texteParag[3] = "Antoine la Boule. Ayant pris de l'avance, Kim Pendragon se retourna";
+    tableau[numT].texteParag[4] = "pour attendre sa suite. À sa grande surprise, le sentier derrière ";
+    tableau[numT].texteParag[5] = "elle était désert! Antoine et Joseph avaient disparu!";
+    tableau[numT].texteChoix[0] = "1. Mais c'est terrible! Que va-t-il se passer?";
+    tableau[numT].texteChoix[1] = "";
+    tableau[numT].texteChoix[2] = "";
+    tableau[numT].texteChoix[3] = "";
+    tableau[numT].texteChoix[4] = "";
+
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+    // Definition du tableau 3
+    numT++;
+    tableau[numT].idTableau = numT; // numT = 2
+    tableau[numT].lienChoix[0] = 0;
+    tableau[numT].lienChoix[1] = 4;
+    tableau[numT].lienChoix[2] = 5;
+    tableau[numT].lienChoix[3] = 6;
+    tableau[numT].lienChoix[4] = 0;
+    tableau[numT].titre = "L'aventure commence";
+    tableau[numT].texteParag[0] = "   Kim Pendragon était faite du bois noble des héros de jadis, et";
+    tableau[numT].texteParag[1] = "elle eut tôt fait de retrouver ses esprits. Paniquer était inutile:";
+    tableau[numT].texteParag[2] = "il fallait immédiatement se mettre à la recherche des disparus.";
+    tableau[numT].texteParag[3] = "   Un embranchement apparut sur le sentier. À droite, une sapinière;";
+    tableau[numT].texteParag[4] = "tout droit, une zone de forêt plus dense; à gauche, des arburstres";
+    tableau[numT].texteParag[5] = "tapissant le sol. Sans hésiter, Kim Pendragon …";
+    tableau[numT].texteChoix[0] = "1. ... Prit le chemin de droite. Les sapins, ça sent bon!";
+    tableau[numT].texteChoix[1] = "2. ... Continua tout droit. Il y a sûrement moins de moustiques.";
+    tableau[numT].texteChoix[2] = "3. ... Décida d'aller vers les arbustres. Ils étaient certainement";
+    tableau[numT].texteChoix[3] = "       chargés de petits fruits délicieux!";
+    tableau[numT].texteChoix[4] = "";
+
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+    // Definition du tableau 4
+    numT++;
+    tableau[numT].idTableau = numT; // numT = 3
+    tableau[numT].lienChoix[0] = 0;
+    tableau[numT].lienChoix[1] = 7;
+    tableau[numT].lienChoix[2] = 3;
+    tableau[numT].lienChoix[3] = 5;
+    tableau[numT].lienChoix[4] = 0;
+    tableau[numT].titre = "Cul de sac!";
+    tableau[numT].texteParag[0] = "   Au bout de quelques mètres, Kim Pendragon dut s'arrêter. La sapi-";
+    tableau[numT].texteParag[1] = "nière bordait une falaise infranchissable, de sorte qu'il était seul-";
+    tableau[numT].texteParag[2] = "ement possible de faire demi-tour. Aperçevait-elle une légère ouver-";
+    tableau[numT].texteParag[3] = "ture sur la gauche?";
+    tableau[numT].texteParag[4] = "   Au sol, près d'un sapin vieux et torturé, poussaient des champi-";
+    tableau[numT].texteParag[5] = "gnons d'un bleu clair desquels émanait une forte aura magique ... ";
+    tableau[numT].texteChoix[0] = "1. Se penchant, Kim Pendragon saisit un champignon et y mordit à ";
+    tableau[numT].texteChoix[1] = "   belles dents.";
+    tableau[numT].texteChoix[2] = "2. Ignorant les champignons mystérieux, Kim Pendragon fit demi-tour.";
+    tableau[numT].texteChoix[3] = "3. Au prix de quelques écorchures, Kim Pendragon se faufila entre ";
+    tableau[numT].texteChoix[4] = "   deux sapins sur la gauche.";
+
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+    // Definition du tableau 5
+    numT++;
+    tableau[numT].idTableau = numT; // numT = 4
     tableau[numT].lienChoix[0] = 1;
     tableau[numT].lienChoix[1] = 2;
     tableau[numT].lienChoix[2] = 3;
     tableau[numT].lienChoix[3] = 4;
     tableau[numT].lienChoix[4] = 5;
-    tableau[numT].titre = "Prologue";
-    tableau[numT].texteParag[0] = "   Texte du chapitre. Cette partie doit \210tre bien format\202e et surtout";
-    tableau[numT].texteParag[1] = "ne pas contenir d'accents ou de caract\212res weird. Kim Pendragon se";
-    tableau[numT].texteParag[2] = "promenait dans la for\210t en fredonnant un air a la mode. Soudain, son";
-    tableau[numT].texteParag[3] = "attention fut retenue par un bruit \202trange provenant d'un buisson sur";
-    tableau[numT].texteParag[4] = "sa gauche.";
+    tableau[numT].titre = "Un arbre barre la route";
+    tableau[numT].texteParag[0] = "   Un arbre massif empêchait toute progression en ce sens. Trois";
+    tableau[numT].texteParag[1] = "possibilités ";
+    tableau[numT].texteParag[2] = "";
+    tableau[numT].texteParag[3] = "";
+    tableau[numT].texteParag[4] = "";
     tableau[numT].texteParag[5] = "";
-    tableau[numT].texteChoix[0] = "1. N'\202coutant que son courage, elle \202carte les branches ..";
-    tableau[numT].texteChoix[1] = "2. Consid\202rant la possible pr\202sence d'araign\202es, elle vaporise";
-    tableau[numT].texteChoix[2] = "   une quantit\202 d\202raisonable de Raid dans le buisson ..";
-    tableau[numT].texteChoix[3] = "3. Interdite, elle recule lentement sans regarder derri\212re elle ..";
+    tableau[numT].texteChoix[0] = "";
+    tableau[numT].texteChoix[1] = "";
+    tableau[numT].texteChoix[2] = "";
+    tableau[numT].texteChoix[3] = "";
     tableau[numT].texteChoix[4] = "";
+
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+    // Definition du tableau 6
+    numT++;
+    tableau[numT].idTableau = numT; // numT = 5
+    tableau[numT].lienChoix[0] = 1;
+    tableau[numT].lienChoix[1] = 2;
+    tableau[numT].lienChoix[2] = 3;
+    tableau[numT].lienChoix[3] = 4;
+    tableau[numT].lienChoix[4] = 5;
+    tableau[numT].titre = "";
+    tableau[numT].texteParag[0] = "";
+    tableau[numT].texteParag[1] = "";
+    tableau[numT].texteParag[2] = "";
+    tableau[numT].texteParag[3] = "";
+    tableau[numT].texteParag[4] = "";
+    tableau[numT].texteParag[5] = "";
+    tableau[numT].texteChoix[0] = "";
+    tableau[numT].texteChoix[1] = "";
+    tableau[numT].texteChoix[2] = "";
+    tableau[numT].texteChoix[3] = "";
+    tableau[numT].texteChoix[4] = "";
+
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+    // Definition du tableau 7
+    numT++;
+    tableau[numT].idTableau = numT; // numT = 6
+    tableau[numT].lienChoix[0] = 1;
+    tableau[numT].lienChoix[1] = 2;
+    tableau[numT].lienChoix[2] = 3;
+    tableau[numT].lienChoix[3] = 4;
+    tableau[numT].lienChoix[4] = 5;
+    tableau[numT].titre = "";
+    tableau[numT].texteParag[0] = "";
+    tableau[numT].texteParag[1] = "";
+    tableau[numT].texteParag[2] = "";
+    tableau[numT].texteParag[3] = "";
+    tableau[numT].texteParag[4] = "";
+    tableau[numT].texteParag[5] = "";
+    tableau[numT].texteChoix[0] = "";
+    tableau[numT].texteChoix[1] = "";
+    tableau[numT].texteChoix[2] = "";
+    tableau[numT].texteChoix[3] = "";
+    tableau[numT].texteChoix[4] = "";
+
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+    // Definition du tableau 8
+    numT++;
+    tableau[numT].idTableau = numT; // numT = 7
+    tableau[numT].lienChoix[0] = 1;
+    tableau[numT].lienChoix[1] = 2;
+    tableau[numT].lienChoix[2] = 3;
+    tableau[numT].lienChoix[3] = 4;
+    tableau[numT].lienChoix[4] = 5;
+    tableau[numT].titre = "";
+    tableau[numT].texteParag[0] = "";
+    tableau[numT].texteParag[1] = "";
+    tableau[numT].texteParag[2] = "";
+    tableau[numT].texteParag[3] = "";
+    tableau[numT].texteParag[4] = "";
+    tableau[numT].texteParag[5] = "";
+    tableau[numT].texteChoix[0] = "";
+    tableau[numT].texteChoix[1] = "";
+    tableau[numT].texteChoix[2] = "";
+    tableau[numT].texteChoix[3] = "";
+    tableau[numT].texteChoix[4] = "";
+
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+    // Definition du tableau 9
+    numT++;
+    tableau[numT].idTableau = numT; // numT = 8
+    tableau[numT].lienChoix[0] = 1;
+    tableau[numT].lienChoix[1] = 2;
+    tableau[numT].lienChoix[2] = 3;
+    tableau[numT].lienChoix[3] = 4;
+    tableau[numT].lienChoix[4] = 5;
+    tableau[numT].titre = "";
+    tableau[numT].texteParag[0] = "";
+    tableau[numT].texteParag[1] = "";
+    tableau[numT].texteParag[2] = "";
+    tableau[numT].texteParag[3] = "";
+    tableau[numT].texteParag[4] = "";
+    tableau[numT].texteParag[5] = "";
+    tableau[numT].texteChoix[0] = "";
+    tableau[numT].texteChoix[1] = "";
+    tableau[numT].texteChoix[2] = "";
+    tableau[numT].texteChoix[3] = "";
+    tableau[numT].texteChoix[4] = "";
+
     //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 
     return 0;
